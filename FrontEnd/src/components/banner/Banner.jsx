1fdf34b88a5633e3a0458c8feff195fcd2addbf9
VaibhavxD11 <dubeyvaibhav16@gmail.com>
2023-10-13 14:45:21 +0530

    New Project

---
+++ FrontEnd/src/components/banner/Banner.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import bannerImg from "../../assets/img/banner-img.png";
+
+import { useGlobalContext } from "../../context/context";
+
+
+import "./banner.css";
+
+const Banner = () => {
+  const { openSidebar } = useGlobalContext();
+
+  return (
+    <section className="banner d-flex align-items-center ">
+      <div className="bubble-animation">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 ">
+            <h2 className="theme-clr mb-3 fs-5">
+              An investment in Knowledge pays the best interest
+            </h2>
+            <h1 className="mb-3 text-capitalize fs-3 fw-bold">
+              best online platform for learning.
+            </h1>
+            <p className="mb-4 text-muted">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod
+              nisi delectus a minus.
+            </p>
+            
+            <Link to="/login">
+              <button type="button" className="theme-btn">
+                join free
+              </button>
+            </Link>
+            
+          </div>
+          <div className="col-md-6 order-first order-md-last mb-5 mb-md-0">
+            <div className="circular-img">
+              <img src={bannerImg} alt="banner img" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
